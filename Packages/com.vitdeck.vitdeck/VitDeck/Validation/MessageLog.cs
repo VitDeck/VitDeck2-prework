01456c9e08aebc3b398e392ce6a446a1ae4c7d54
Sokuhatiku <sokuhatiku@gmail.com>
2022-09-18 03:01:11 +0900

    ログにタイプを反映

--- Packages/com.vitdeck.vitdeck/VitDeck/Validation/MessageLog.cs
+++ Packages/com.vitdeck.vitdeck/VitDeck/Validation/MessageLog.cs
@@ -2,16 +2,18 @@
 {
     public class MessageLog : LogBase
     {
-        public override LogType Type => LogType.Info;
-        
+        private LogType type;
+        public override LogType Type => type;
+
         private string message;
         public override string Message => message;
         public override bool HasInformation => false;
         public override bool HasSolution => false;
-        
-        public static MessageLog Create(LogType logType, string message)
+
+        public static MessageLog Create(LogType type, string message)
         {
             var instance = CreateInstance<MessageLog>();
+            instance.type = type;
             instance.message = message;
             return instance;
         }
